51ecf87eb31ea248e28c52c5d8f4cec7f21d941d
nikhil254 <nikhil.sachan@mindruby.com>
2024-04-24 15:24:52 +0530

    potgres chnages

--- backend/config/supertoken.js
+++ backend/config/supertoken.js
@@ -6,7 +6,6 @@ import Dashboard from "supertokens-node/recipe/dashboard/index.js";
 const superTokenConfig = {
     supertokens: {
         connectionURI: "http://localhost:3567",
-        // apiKey: "someKey" // OR can be undefined
     },
     appInfo: {
         apiDomain: "http://localhost:3000",
@@ -14,40 +13,9 @@ const superTokenConfig = {
         websiteDomain: "http://localhost:4200"
     },
     recipeList: [
-        EmailPassword.init({
-        override: {
-            functions: (originalImplementation) => {
-                return {
-                    ...originalImplementation,
-                    signUp: async function (input) {
-                        // First we call the original implementation of signUp.
-                        let response = await originalImplementation.signUp(input);
-                        console.log(response);
-                        // Post sign up response, we check if it was successful
-                        if (response.status === "OK" && response.user.loginMethods.length === 1 && input.session === undefined) {
-                            /**
-                            * 
-                            * response.user contains the following info:
-                            * - emails
-                            * - id
-                            * - timeJoined
-                            * - tenantIds
-                            * - phone numbers
-                            * - third party login info
-                            * - all the login methods associated with this user.
-                            * - information about if the user's email is verified or not.
-                            * 
-                            */
-                            // TODO: post sign up logic
-                        }
-                        return response;
-                    }
-                }
-            }
-        }
-    }),
-    Session.init(),
-    Dashboard.init(),
+        EmailPassword.init(),
+        Session.init(),
+        Dashboard.init(),
     ]
 };
 
